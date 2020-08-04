8fc487ee072e20fdbd8270530150f4c46c5329ab
elmernocon <elmernocon@gmail.com>
2020-08-04 14:50:21 +0800

    Added LeanTween support.

---
+++ PluginSupport/LeanTween/LTAnimations.cs
@@ -0,0 +1,9 @@
+namespace Fopoon.ScriptBasedAnimations.LeanTweenSupport
+{
+    /// <summary>
+    /// A collection of LeanTween animations.
+    /// </summary>
+    public static partial class LTAnimations
+    {
+    }
+}
